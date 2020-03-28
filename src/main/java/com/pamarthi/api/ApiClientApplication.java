2dba41aa308ce1e64d2b28217bc07e4fd5cc6834
Durga Rao Pamarthi <durga.pamarthi@iddinkgroup.com>
2020-03-28 17:18:02 +0530

    first commit

--- src/main/java/com/pamarthi/api/ApiClientApplication.java
+++ src/main/java/com/pamarthi/api/ApiClientApplication.java
@@ -18,24 +18,11 @@ package com.pamarthi.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiClientApplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(ApiClientApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("*").allowedOrigins("*");
-			}
-		};
 	}
 }
